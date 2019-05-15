fa5a23dda6c1cd9b66c400ce9ee66cd66a5a215b
Eduardo Cabrera <e.cabreralopez@ugto.mx>
2019-05-15 17:35:44 -0500

    Add files via upload

--- MBGRSD.X/Main.c
+++ MBGRSD.X/Main.c
@@ -1,6 +1,6 @@
 #include "mcc_generated_files/mcc.h"
 #include "ff.h"
-#include "lcd_lib.h"
+//#include "lcd_lib.h"
 #include "Errores.h"
 
 FATFS FatFs;
@@ -24,7 +24,7 @@ void main(void)
     if (f_mount(&FatFs, "", 1) != FR_OK) {	/* Inicializa SD */
         Error(1);
         __delay_sec(2);
-        
+        Error(99);
         while(f_mount(&FatFs, "", 1) != FR_OK) {
             ;
         }
@@ -32,24 +32,31 @@ void main(void)
     
     Error(2);
     __delay_sec(2);
+    Error(99);
         
     if (f_open(&Fil, "BeeDev.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE) == FR_OK) {	/* Abre o crea el archivo */
         Error(3);
+        
+       
         __delay_sec(2);
+         Error(99);
             
 			if ((Fil.fsize != 0) && (f_lseek(&Fil, Fil.fsize) != FR_OK)) goto endSD;	/* Salta al final del archivo */
                 Error(4);
                 __delay_sec(2);
                 
-                f_write(&Fil, "ESTAS MUERTO.\r\n", 46, &bw);	/* ]Escribe en el archivo */
+                Error(99);
+                f_write(&Fil, "CREADO POR MI :V .\r\n", 46, &bw);	/* ]Escribe en el archivo */
                 Error(5);
                 __delay_sec(2);
-                
+                 Error(99);
                 endSD: f_close(&Fil);								/* Cierra el archivo */
                 Error(6);
                 __delay_sec(1);
+                
                 Error(7);
                 __delay_sec(2);
+                 Error(99);
      
 	}
     else {
