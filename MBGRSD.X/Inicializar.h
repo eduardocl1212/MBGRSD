269ecbc50fea4c5fe1d2b503ef752271d1318bd8
Eduardo Cabrera <e.cabreralopez@ugto.mx>
2019-05-15 16:43:13 -0500

    Add files via upload

---
+++ MBGRSD.X/Inicializar.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   Inicializar.h
+ * Author: eduar
+ *
+ * Created on 9 de mayo de 2019, 01:09 PM
+ */
+
+#ifndef INICIALIZAR_H
+#define	INICIALIZAR_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* INICIALIZAR_H */
+
