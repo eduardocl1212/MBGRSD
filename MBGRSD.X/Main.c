e88f627e71446cfb060820091d66428e14c9300e
Eduardo Cabrera <e.cabreralopez@ugto.mx>
2019-06-03 08:33:23 -0500

    UPDATE NEW CODE
    MODIFICADO

--- MBGRSD.X/Main.c
+++ MBGRSD.X/Main.c
@@ -1,6 +1,15 @@
+/*
+ * File:   main.c
+ * Author: big_m
+ *
+ * Created on 25 de mayo de 2019, 06:32 PM
+ */
+
+
 #include "mcc_generated_files/mcc.h"
 #include "ff.h"
 #include "Errores.h"
+#include "init.h"
 
 #include <xc.h>
 #include <stdio.h>
@@ -15,14 +24,14 @@ float volt;
 unsigned int adcin;
 
 UINT bw;
-
+UINT br;
 int a = 0;
-int out;
 char ND[2];
-char * data;
-char * buffor = "!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~¹¶ñÑã \r\n"; //Pues necesitamos un buffer o mas bien un arreglo con el tamaño adecuado para la lectura de la tarjeta SD
+int b = 0;
+int push;
 
-void send_string(const char *x){       //Pues crei que al hacer uso del puerto serial podria leer que contiene la tarjeta SD pero pues no fue asi, pero pronto encontrare la utilidad
+char data;
+void send_string(const char *x){
     while(*x){
         __delay_ms(1);
         EUSART1_Write(*x++);
@@ -30,81 +39,93 @@ void send_string(const char *x){       //Pues crei que al hacer uso del puerto s
 }
 FATFS FatFs;
 FIL Fil;
-int i = -5000;
 
-
-void Init8LEDs(void);           //Función Inicializa puerto A
-int interruptadc(int,int);
-void grabador(void);
-void reproductor(void);
-void guardar(void);
+void initSD(void);
+int interruptadc(int);
+void grabadorSD(void);
+void reproductorSD(void);
+void guardado(void);
 void __delay_sec(char sec);
+void lectura(void);
 
 
 void main(void)
 {
     // Initialize the device
     SYSTEM_Initialize();
-   
+    
     OSCCON = 0x53;      //Oscilador interno 4 MHz
-    ADCON2 = 0x94;      //ACQT = 4TAD, TAD de 1microS, Justificado derecha 
+    ADCON2 = 0x14;      //ACQT = 4TAD, TAD de 1microS, Justificado izquierda 
     ADCON1 = 0x00;      //Vref- = GND, Vref+ = +5V
     LATA = 0x00;        //Limpiamos A
     TRISA = 0x00;       //bit RA1 como salida 
     ANSELA = 0x00;      //RA1 como analógico
-    TRISC = 0x04;       //bit RC2 como entrada
+    TRISC = 0x07;       //bit RC2, RC1, RC0 como entrada
     ANSELC = 0x04;      //RC2 como analógico
     ADCON0 = 0x39;      //Enciendo ADC, canal AN14
+    
     GIE = 1;            //Habilita interrupciones globales
     PEIE = 1;           //Habilita interrupciones de periféricos
     ADIE = 1;           //Habilita interrupción ADC
-       guardar();
+   
+    //initSD(); //Inicializa MicroSD
     while(1){
-        //grabador();
-       LATA = 0x00;
-        Error(55);
+       Error(0);
+       push =  PORTC;
+       push = 0x03 & push;
+       if(push == 2) guardado();   //RC1 GUARDA 
+       if(push == 1) lectura(); //RC0 REPRODUCE
     }
+    
     return;
 }
 
-int interruptadc(int adcin, int a) {                //Interrupción conversión                       
-    if(ADIF==1) {                     //verifica bandera conversión ADC
-        ADIE=0;                       //Deshabilita interrupción ADC
-        adcin = (ADRESH<<8)+ADRESL;   //Asigna valor ADRESH y ADRESL a adcin
+int interruptadc(int adcin) {               //Comienza conversion ANALOGICA DIGITAL                    
+    if(ADIF==1) {                     
+        ADIE=0;                       
+        adcin = (ADRESH<<8)+ADRESL;   
     }
-    ADIE=1;                           //Habilita interrupción ADC
-    ADIF=0; //Limpia bandera conversión ADC
-    //data[a] = adcin/4;
+    ADIE=1;                           
+    ADIF=0; 
     return adcin;
 }
-void grabador(void){
-       Error(3);            //Inicia conversión ADC
-    do{
-        i++;        
-        ADCON0bits.GO=1;
-        adcin = interruptadc(adcin, i);
-        PORTA = adcin/4;            //Asigna valor adcin a puerto A
-        volt = (adcin*_vin)/_base;  //Conversión a flotante
-        ND[0] = (char) adcin;
-        //ND[1] = (char) adcin;
-        f_write(&Fil, ND , 2 , &bw);
-        send_string(ND);
-        }while(i!=5000);
-       return; 
+
+void grabadorSD(void){
+    Error(9); //Inicia Grabacion
+    int j = 0;
+    int a = 0;    
+        j = 0;
+        do{            
+            ADCON0bits.GO=1;
+            a = ADRESH; 
+            ND[0] = (char) a;
+            ND[1] = (char) a;
+            f_write(&Fil, ND , 2 , &bw);
+            //send_string(ND);
+            j++;
+        }while(j!=30000);   
+         Error(4);
+    return; 
 }
 
-void reproductor(void){
-    i = 0;
-    f_read(&Fil, data, 2 , &bw);
+void reproductorSD(void){
+    Error(8); //Inicia conversión ADC
+    int j = 0;
+    char c;    
     do{
-        i++;
-        adcin =  data[i];
-        PORTA = adcin;        
-        __delay_ms(10);
-    }while(i!=5000);
-        //Display7Seg(volt);          //Llama a función desplegar en displays
-        //LA1=out;
-         //IO_RA1_SetLow();
+        f_read(&Fil, ND , 2 , &br);
+        c = ND[0];
+        b = (int) c;
+        PORTA = b;
+        for(int i=0;i<3;i++);
+        c = ND[1];
+        b = (int) c;
+        PORTA = b;
+        for(int i=0;i<2;i++);
+        j++;
+        }while(j!=30000);
+        Error(4);
+    return; 
 }
 
 
@@ -114,45 +135,55 @@ void __delay_sec(char sec) {
     }
 }
 
-void guardar(void){
-    SYSTEM_Initialize();
+void initSD(void){
     if (f_mount(&FatFs, "", 1) != FR_OK) {	/* Inicializa SD */
-        
         Error(33);
-        __delay_sec(2);
+        __delay_sec(1);
         while(f_mount(&FatFs, "", 1) != FR_OK) {
             ;
         }
     }    
     
     Error(1);
-    __delay_sec(2);
+    __delay_sec(1);
         
     if (f_open(&Fil, "BeeDev.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE) == FR_OK) {	/* Abre o crea el archivo */
         Error(2);
-        
-       
-        __delay_sec(2);
-         Error(3);
-            
-			if ((Fil.fsize != 0) && (f_lseek(&Fil, Fil.fsize) != FR_OK)) goto endSD;	/* Salta al final del archivo */
-                Error(4);
-                __delay_sec(2);
-                
-                //grabador();	// DATA ARRAY, NUMERO DE CHAR
-                reproductor();
-                //f_printf(&fil, "%s", "String");  
-                Error(5);
-                __delay_sec(2);
-                endSD: f_close(&Fil);								/* Cierra el archivo */
-                Error(6);
-                __delay_sec(1);
-                
-                Error(7);
-                __delay_sec(2);
-     
-	}
+        __delay_sec(1);
+    	}
     else {
         Error(0);
     }
+            
+}
+
+void guardado(void){
+    initSD();
+    if ((Fil.fsize != 0) && (f_lseek(&Fil, Fil.fsize) != FR_OK))	/* Salta al final del archivo */
+    Error(3);
+    __delay_sec(1);
+    //Error(999);
+    grabadorSD();	// DATA ARRAY, NUMERO DE CHAR
+    //f_printf(&fil, "%s", "String");  
+    Error(4);
+    __delay_sec(1);
+    Error(5);
+    __delay_sec(1);
+    f_close(&Fil);								/* Cierra el archivo */
+    Error(6);
+    __delay_sec(1);
+
+
+}
+
+void lectura(void){
+    initSD();
+    reproductorSD();	// DATA ARRAY, NUMERO DE CHAR
+    //f_printf(&fil, "%s", "String");  
+    Error(5);
+    __delay_sec(1);
+    f_close(&Fil);								/* Cierra el archivo */
+    Error(6);
+    __delay_sec(1);
+    LATA= 0x00;
 }
